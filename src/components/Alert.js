46eddbc7198f1b177892303c1161c7d38ae911eb
ishaparashar06 <ishaparashar06@gmail.com>
2024-09-27 21:36:46 +0530

    logic of word count and button positions updated

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Alert(props) {
   return (
-    <div>
+    <div style={{height: '50px'}}>
         {props.alert && <div class={`alert alert-${props.alert.type}`} role="alert">
             <strong>{props.alert.msg}</strong>
         </div>}
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -47,17 +47,17 @@ export default function TextForm(props) {
           <div className="mb-3">
             <textarea className="form-control" id="myBox" placeholder="Enter text here" value={text}  onChange={handleOnChange} rows="8" style={{backgroundColor:(props.mode==='light')? '#dae5f8': '#637697', color:(props.mode==='light')? 'black': 'white' } }></textarea>
           </div>
-          <button className="btn btn-primary mx-1" onClick={handleClearText}>Clear Text</button>
-          <button className="btn btn-primary mx-1" onClick={handleUpClick} >Convert to Uppercase</button>
-          <button className="btn btn-primary mx-1" onClick={handleLoClick} >Convert to Lowercase</button>
-          <button className="btn btn-primary mx-1" onClick={capitalize}>Capitalize</button>
-          <button className="btn btn-primary mx-1" onClick={speak}>Speak Aloud</button>
+          <button className="btn btn-primary mx-1 my-1" onClick={handleClearText}>Clear Text</button>
+          <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick} >Convert to Uppercase</button>
+          <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick} >Convert to Lowercase</button>
+          <button className="btn btn-primary mx-1 my-1" onClick={capitalize}>Capitalize</button>
+          <button className="btn btn-primary mx-1 my-1" onClick={speak}>Speak Aloud</button>
           
         </div>
         <div className="container my-3" style={{color:(props.mode==='light')? 'black': 'white'}}>
           <h2>Your Text Summary</h2>
-          <p>{text.split(" ").length-1} words and {text.length} characters</p>
-          <p>{0.008 * (text.split(" ").length-1)} minutes read</p>
+          <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+          <p>{0.008 * (text.split(" ").filter((element)=>{return element.length!==0}).length)} minutes read</p>
           <h2>Preview</h2>
           <p>{text.length>0?text:"Enter something in the textbox to preview here"}</p>
         </div>
